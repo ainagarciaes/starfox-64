bad373cf07f2c7e8e71b92c8678716a94af7eb53
ainagarciaes <ainaa.105@gmail.com>
2020-05-04 11:07:49 +0200

    credits

---
+++ StarFoxUnity/Assets/timeToMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class timeToMenu : MonoBehaviour
+{
+    private float countdownTime = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownTime -= Time.deltaTime;
+        if (countdownTime <= 0.0f)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
