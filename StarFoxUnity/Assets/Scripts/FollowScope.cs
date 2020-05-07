c9018eac8665584b452e55fd9f37225554c8e88a
jpagesbri <joan.pages.esbri@gmail.com>
2020-05-07 22:48:29 +0200

    BarrelRollV2

--- StarFoxUnity/Assets/Scripts/FollowScope.cs
+++ StarFoxUnity/Assets/Scripts/FollowScope.cs
@@ -12,41 +12,59 @@ public class FollowScope : MonoBehaviour
     // rotation parameters
     private float offset;
     private float offset_ini;
-    private bool rotation_side; //true = right roll, false = left roll
+    private float rotation_side; //true = right roll, false = left roll
     private bool rotating = false;
     private float rotation_step;
+    private float bias;
 
+    private const float rollAcc = 10f;
+    public float duration;
+    private bool rollInitialized;
+    private float rollingSpeed;
+    private float currentRotation;
     // Start is called before the first frame update
     void Start()
     {
+        duration = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        viewportPos = Camera.main.WorldToViewportPoint(transform.position);
-        viewportAim = Camera.main.WorldToViewportPoint(lookAtObject.transform.position);
-        transform.LookAt(lookAtObject.transform.position);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rotating = true;
+            rollInitialized = false;
+            rotation_side = -1;
+            bias = 1;
+            duration = 0;
 
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rotating = true;
+            rollInitialized = false;
+            rotation_side = 1;
+            bias = 1;
+            duration = 0;
+
+        }
         if (rotating)
         {
-            rotate();
-            transform.RotateAround(transform.position, transform.forward, offset);
+            DoABarrelRoll();
+            duration += Time.deltaTime;
         }
-
         else
         {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                offset_ini = 50 * (viewportPos.x - viewportAim.x);
-                rotation_step = 0;
-                rotating = true;
-            }
-            transform.RotateAround(transform.position, transform.forward, 50 * (viewportPos.x - viewportAim.x));
-        }
+            print(duration);
 
-        distance = viewportAim - viewportPos;
-        transform.position = Camera.main.ViewportToWorldPoint(viewportPos + new Vector3(distance.x, distance.y - 0.1f, 0) * 0.005f);
+            viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+            viewportAim = Camera.main.WorldToViewportPoint(lookAtObject.transform.position);
+            transform.LookAt(lookAtObject.transform.position);
+            transform.RotateAround(transform.position, transform.forward, 50 * (viewportPos.x - viewportAim.x));
+            distance = viewportAim - viewportPos;
+            transform.position = Camera.main.ViewportToWorldPoint(viewportPos + new Vector3(distance.x, distance.y - 0.1f, 0) * Time.deltaTime);
+        }
     }
 
     private void rotate()
@@ -62,5 +80,53 @@ public class FollowScope : MonoBehaviour
             return;
         }
         offset = diff * 360 + offset_ini;
+    }
+
+    private void DoABarrelRoll()
+    {
+        if (rollInitialized)
+        {
+            if (currentRotation < 180)
+                rollingSpeed += Time.deltaTime * rollingSpeed * rollAcc;
+            else if (currentRotation < 390 && rollingSpeed >= 1)
+                rollingSpeed -= Time.deltaTime * rollingSpeed * rollAcc;
+            else
+            {
+                print("stop rotation");
+                rotating = false;
+                rollingSpeed = 0;
+                offset = 0;
+                return;
+            }
+            currentRotation += rollingSpeed;
+            transform.RotateAround(transform.position, transform.forward, -rotation_side * rollingSpeed);
+        }
+        else EnterRoll();
+    }
+
+    private void EnterRoll()
+    {
+        transform.LookAt(Camera.main.ViewportToWorldPoint(
+                            new Vector3((1 - bias) * viewportPos.x + bias * viewportAim.x,
+                                        (1 - bias) * viewportPos.y + bias * viewportAim.y, 
+                                        viewportAim.z)), 
+                        transform.up);
+        if (bias > 0.05f)
+            bias *= Mathf.Pow(0.9f, 500 * Time.deltaTime);
+        else
+        {
+            bias = 0;
+            Vector3 diff2 = transform.parent.transform.up - transform.up;
+            float dist = Vector3.Distance(Vector3.zero, diff2);
+            if (dist > 0.2f) transform.up += 20f * Time.deltaTime * (diff2);
+            else
+                transform.up = transform.parent.transform.up;
+            if (dist <= 0.2f)
+            {
+                currentRotation = 0;
+                rollingSpeed = 1;
+                rollInitialized = true;
+            }
+        }
     }
 }
--- StarFoxUnity/Assets/Scripts/MoveScope.cs
+++ StarFoxUnity/Assets/Scripts/MoveScope.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MoveScope : MonoBehaviour
 {
+    private Vector3 viewportPos;
+    private Vector3 viewportAim;
+    private Vector2 distance;
+
     public float screenWidth = 14;
     public float screenHeight = 10;
     public float min = -0.50f;
@@ -11,19 +15,70 @@ public class MoveScope : MonoBehaviour
     public float mouseX;
     public float mouseY;
     public float xyspeed = 18;
+    public float rollInstant;
+    private float bias;
+
+
+    [SerializeField] GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        rollInstant = 0;
         Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            rollInstant = 0.5f; bias = 1;
+        }
+        if (rollInstant <= 0) Move();
+        else
+        {
+            viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+            viewportAim = Camera.main.WorldToViewportPoint(player.transform.position);
+            rollInstant -= Time.deltaTime;
+            if (rollInstant > 0.4f) CenterScopeLocation();
+            else TweenScopeLocation();
+        }
+
+
+    }
+
+    private void CenterScopeLocation()
+    {
+        distance = viewportAim - viewportPos;
+
+        transform.position = Camera.main.ViewportToWorldPoint(viewportPos + new Vector3(distance.x, distance.y + 0.1f, 0) * 25*Time.deltaTime);
+
+        if (bias > 0.05f)
+            bias *= Mathf.Pow(0.999f, Time.deltaTime);
+        else
+        {
+            bias = 0;
+        }
+    }
+    private void TweenScopeLocation()
+    {
+        //mouseX = Input.GetAxis("Mouse X") / Screen.width;
+        //mouseY = Input.GetAxis("Mouse Y") / Screen.height;
+        //float newx = (Input.mousePosition.x / Screen.height) - 0.5f;
+        //float newy = (Input.mousePosition.y / Screen.height) - 0.5f;
+        //newx = screenWidth * Mathf.Clamp(newx, min, max);
+        //newy = screenHeight * Mathf.Clamp(newy, min, max);
+        //distance.x = newx - transform.localPosition.x;
+        //distance.y = newy - transform.localPosition.y;
+        //transform.localPosition = transform.localPosition + new Vector3(distance.x, distance.y, 0) * 0.1f;
+    }
+
+    private void Move()
+    {
         mouseX = Input.GetAxis("Mouse X") / Screen.width;
         mouseY = Input.GetAxis("Mouse Y") / Screen.height;
-        float newx = (Input.mousePosition.x / Screen.height) - 0.5f;
-        float newy = (Input.mousePosition.y / Screen.height) - 0.5f;
+        float newx = transform.localPosition.x/screenWidth+mouseX*20;
+        float newy = transform.localPosition.y/screenHeight+mouseY*20;
         newx = screenWidth * Mathf.Clamp(newx, min, max);
         newy = screenHeight * Mathf.Clamp(newy, min, max);
         transform.localPosition = new Vector3(newx, newy, transform.localPosition.z);
