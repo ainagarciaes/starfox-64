79bf5b42787688f9083a197e98709c9a870bc494
ainagarciaes <ainaa.105@gmail.com>
2020-05-09 09:49:18 +0200

    destroy script ready to test

---
+++ StarFoxUnity/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public bool destroyable;
+    public int givenScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DestroyObjectFunc()
+    {
+        if (destroyable)
+        {
+            LevelManager.Instance.UpdateScore(givenScore);
+            Destroy(transform.gameObject);
+        }
+    }
+}
