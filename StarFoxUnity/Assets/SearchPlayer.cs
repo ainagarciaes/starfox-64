d854d2747200b4811d7279e0db00fb293c499253
jpagesbri <joan.pages.esbri@gmail.com>
2020-05-13 19:00:32 +0200

    satellite looks at player smoothly

--- StarFoxUnity/Assets/SearchPlayer.cs
+++ StarFoxUnity/Assets/SearchPlayer.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class SearchPlayer : MonoBehaviour
 {
-    GameObject player;
+    Transform player;
+    [SerializeField] Transform partToRotate;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var lookPos = player.transform.position - transform.position;
-        //lookPos.y = 0;
-        var rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 120);
+        //var lookPos = player.transform.position - transform.position;
+        ////lookPos.y = 0;
+        //var rotation = Quaternion.LookRotation(lookPos);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 120);
+
+        Vector3 lookDir = player.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(lookDir);
+        //Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime).eulerAngles;
+        //partToRotate.rotation = Quaternion.Euler(rotation.x, 0, 0);
+        //rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime).eulerAngles;
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime*0.5f);
     }
 }
