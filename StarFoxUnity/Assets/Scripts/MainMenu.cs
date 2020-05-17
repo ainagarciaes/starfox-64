035f795fc6d0acad57dce581ddffa7044d10fdb0
ainagarciaes <ainaa.105@gmail.com>
2020-05-17 16:07:23 +0200

    buttons menu principal done

--- StarFoxUnity/Assets/Scripts/MainMenu.cs
+++ StarFoxUnity/Assets/Scripts/MainMenu.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+
+    GameObject audioTitle;
+
     private void Start()
     {
-        
+        audioTitle = GameObject.Find("AudioClicks");
     }
     // Update is called once per frame
     void Update()
@@ -16,22 +19,26 @@ public class MainMenu : MonoBehaviour
 
     public void InitGame()
     {
+        audioTitle.GetComponent<AudioManager>().PlaySound();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void InitLevel2()
     {
+        audioTitle.GetComponent<AudioManager>().PlaySound();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void CreditsScene()
     {
+        audioTitle.GetComponent<AudioManager>().PlaySound();
         Cursor.visible = false;
         SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
     {
+        audioTitle.GetComponent<AudioManager>().PlaySound();
         Application.Quit();
     }
 }
