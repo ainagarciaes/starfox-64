0a698da07c90be22aa6a8235305ba468d96465a0
ainagarciaes <ainaa.105@gmail.com>
2020-05-18 13:57:06 +0200

    terrains stuff

--- StarFoxUnity/Assets/Scripts/PaseMenuController.cs
+++ StarFoxUnity/Assets/Scripts/PaseMenuController.cs
@@ -8,40 +8,46 @@ public class PaseMenuController : MonoBehaviour
 {
     Transform menu;
     Transform instr;
-
+    GameObject audioPause;
     void Start()
     {
         menu = this.gameObject.transform.GetChild(1);
         instr = this.gameObject.transform.GetChild(2);
+        audioPause = GameObject.Find("AudioPause");
         ToPauseMenu();
     }
 
     public void ToInstructionsMenu()
     {
+        audioPause.GetComponent<AudioManager>().PlaySound();
         menu.gameObject.SetActive(false);
         instr.gameObject.SetActive(true);
     }
 
     public void ToMainMenu()
     {
+        audioPause.GetComponent<AudioManager>().PlaySound();
         Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
 
     public void RestartLevel()
     {
+        audioPause.GetComponent<AudioManager>().PlaySound();
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void ResumeGame()
     {
+        audioPause.GetComponent<AudioManager>().PlaySound();
         Time.timeScale = 1;
         transform.gameObject.SetActive(false);
     }
 
     public void ToPauseMenu()
     {
+        audioPause.GetComponent<AudioManager>().PlaySound();
         menu.gameObject.SetActive(true);
         instr.gameObject.SetActive(false);
     }
