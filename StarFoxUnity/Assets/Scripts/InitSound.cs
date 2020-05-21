e6dbbd829977f7acebf82597ef14bdcfa0529fb9
jpagesbri <joan.pages.esbri@gmail.com>
2020-05-21 14:43:20 +0200

    satellitePrefab

--- StarFoxUnity/Assets/Scripts/InitSound.cs
+++ StarFoxUnity/Assets/Scripts/InitSound.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class InitSound : MonoBehaviour
 {
+    public bool oneShot = false;
     // Start is called before the first frame update
     void Start()
     {
-        this.gameObject.GetComponent<AudioManager>().PlaySound();
+        if (oneShot) 
+            this.gameObject.GetComponent<AudioManager>().PlaySingleSound(0);
+        else
+            this.gameObject.GetComponent<AudioManager>().PlaySound();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
